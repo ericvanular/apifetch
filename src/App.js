573a45a15713c9f66b2ba095827997cb86367328
Eric Vanular <eric.vanular@gmail.com>
2017-01-12 00:55:36 -0500

    Built in reddit API data fetching

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import fetch from 'isomorphic-fetch'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subreddit: '',
+      posts: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      subreddit: e.target.value
+    })
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch(`https://www.reddit.com/r/${this.state.subreddit}.json`)
+    .then(response => {return response.json()})
+    .then(json => {
+      return this.setState({ posts: json.data.children.map(child => child.data.url)  })
+    })
+    .catch(error => {throw error});
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h2>Welcome to the API fetch machine</h2>
+          <hr/>
+          <form>
+            <input
+              type="textarea"
+              placeholder="Type here to chat!"
+              value={this.state.subreddit}
+              onChange={this.handleChange}
+            />
+            <button onClick={this.handleSubmit}>Submit</button>
+          </form>
+          <hr/>
+          <ul>
+            {this.state.posts.map((post,i) =>
+              <li key={i}>{post}</li>
+            )}
+          </ul>
+          <hr/>
+          To get started, type in a subreddit.
       </div>
     );
   }
